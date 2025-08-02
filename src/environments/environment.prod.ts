b9c20a2768e49753e77e6d96b33a5b7c9a77fc58
Ariel Morada <aimoradajr9@gmail.com>
2025-08-02 15:39:42 +0800

    DEV environment files on prod. vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+// Production environment
 export const environment = {
   production: true,
   apiUrl: 'https://doc-extractor-tool-backend.onrender.com/api',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
+// Default environment (development)
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api',
